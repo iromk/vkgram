960929797cc999bcc357ff818aca0476b6f8f94b
nnm <roman.syrchin@gmail.com>
2018-04-20 14:27:30 +0500

    ADD attached WebJsonProvider as library

--- app/src/main/java/pro/xite/dev/vkgram/MainActivity.java
+++ app/src/main/java/pro/xite/dev/vkgram/MainActivity.java
@@ -1,12 +1,7 @@
 package pro.xite.dev.vkgram;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,13 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
