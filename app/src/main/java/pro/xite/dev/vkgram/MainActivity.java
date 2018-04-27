f7d2b6856e03d891e23f7345550684563616ec22
nnm <roman.syrchin@gmail.com>
2018-04-27 11:32:39 +0500

    ADD applying theme implemented in a draft.

--- app/src/main/java/pro/xite/dev/vkgram/MainActivity.java
+++ app/src/main/java/pro/xite/dev/vkgram/MainActivity.java
@@ -2,7 +2,9 @@ package pro.xite.dev.vkgram;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.StyleRes;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,9 +12,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private @StyleRes int theme = R.style.VkgramThemeGreengo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            theme = savedInstanceState.getInt("A");
+
+        }
+        setTheme(theme);
         setContentView(R.layout.content_main);
     }
 
@@ -28,10 +37,29 @@ public class MainActivity extends AppCompatActivity {
 
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, ThemeSelectActivity.class);
-            startActivityForResult(intent, 0);
+            startActivityForResult(intent, 1);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("A", theme);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.w(TAG, "onActivityResult: ");
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data != null) {
+            final @StyleRes int theme = data.getIntExtra("A", Integer.MIN_VALUE);
+            if(theme != Integer.MIN_VALUE) {
+                this.theme = theme;
+                recreate();
+            }
+        }
     }
 }
--- app/src/main/java/pro/xite/dev/vkgram/ThemeSelectActivity.java
+++ app/src/main/java/pro/xite/dev/vkgram/ThemeSelectActivity.java
@@ -1,13 +1,71 @@
 package pro.xite.dev.vkgram;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.StyleRes;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 public class ThemeSelectActivity extends AppCompatActivity {
+
+    private static final String TAG = ThemeSelectActivity.class.getSimpleName();
+    private @StyleRes int newTheme = Integer.MIN_VALUE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            newTheme = savedInstanceState.getInt("A");
+            if(newTheme != Integer.MIN_VALUE)
+                setTheme(newTheme);
+        }
         setContentView(R.layout.activity_theme_select);
     }
+
+    public void onButtonClick(View view) {
+        newTheme = R.style.VkgramThemeIndigo;
+        recreate();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.w(TAG, "onOptionsItemSelected: ");
+        finish();
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.w(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Override
+    public void finish() {
+        Log.w(TAG, "finish: ");
+        if(newTheme != Integer.MIN_VALUE) {
+            Intent result = new Intent();
+            result.putExtra("A", newTheme);
+            setResult(2, result);
+        }
+        super.finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.w(TAG, "onBackPressed: ");
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("A", newTheme);
+    }
+
+
+
+
 }
