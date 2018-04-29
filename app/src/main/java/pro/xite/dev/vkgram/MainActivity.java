3ac95c1b7f0fbfcfe69f442ebbbcfa6835332665
nnm <roman.syrchin@gmail.com>
2018-04-29 20:00:59 +0500

    ADD toolbar, drawer

--- app/src/main/java/pro/xite/dev/vkgram/MainActivity.java
+++ app/src/main/java/pro/xite/dev/vkgram/MainActivity.java
@@ -1,20 +1,40 @@
 package pro.xite.dev.vkgram;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.drawer_main_layout) DrawerLayout drawerLayoutMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.drawer_main);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayoutMain,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawerLayoutMain.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
     }
 
     @Override
